d32cb8de8b1af20b1ba513c6afb8d1f9bcaff28c
David Llanos <dllanosx@gmail.com>
2021-02-24 11:06:07 -0500

    mostrar pregunta 2 si hay respuesta a pregunta 1 solamente

--- probproy.py
+++ probproy.py
@@ -48,16 +48,19 @@ navbar = dbc.Nav(className="nav nav-pills", children=[
 # Input
 inputs = dbc.FormGroup([
     html.H4("Seleccione tipo de problema"),
-    dcc.RadioItems(id="tipo_problema", options=[{"label":x,"value":x} for x in problemas], value="Personal",
+    dcc.RadioItems(id="tipo_problema", options=[{"label":x,"value":x} for x in problemas], value="",
                    labelStyle={
                     'display': 'block',
                     'margin-right': '7px',
                     'font-weight': 500
                 }),
     html.Br(),
-    html.H4(u"Problema específico"),
+    html.H4(u"Problema específico", id= 'titulo_problema_especifico', style={'display':'none'}),
 
     dcc.RadioItems(id='problema_especifico', value="____",
+                   style= {
+                       'display':'block'
+                   },
                    labelStyle={
                     'display': 'block',
                     'margin-right': '7px',
@@ -109,6 +112,24 @@ app.layout = dbc.Container(fluid=True, children=[
     Input('tipo_problema', 'value'))
 def def_problema_seleccionado(problema_seleccionado):
     return [{'label': i, 'value': i} for i in problemas[problema_seleccionado]]
+
+### OPCIONES DISPONIBLES
+@app.callback(
+    Output('problema_especifico', 'value'),
+    Input('problema_especifico', 'options'))
+def def_problemas_disponibles(opciones_disponibles):
+    return opciones_disponibles[0]['value'] #selected value in the RadioItems
+
+### OCULTAR/MOSTRAR PREGUNTA
+@app.callback(
+    Output('problema_especifico', 'labelStyle'),
+    Output('titulo_problema_especifico', 'style'),
+    [Input('tipo_problema', 'value')])
+def def_mostrar_prob_especifico(mostrar_opciones):
+    if mostrar_opciones != '':
+        return [{'display': 'block'}, {'display': 'block'}]
+    if mostrar_opciones == '':
+        return [{'display': 'none'}, {'display': 'none'}]
 
 ### DESCRIPCION DEL PROBLEMA
 @app.callback(
