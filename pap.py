4039dd68dc09bd6331e38cfe1214aa96c9008069
David Llanos <dllanosx@gmail.com>
2021-03-31 17:52:35 -0400

    version en heroku

--- pap.py
+++ pap.py
@@ -4,24 +4,28 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from dash_html_components.H5 import H5
 
-import dash_table
-import pandas as pd
-import plotly.graph_objects as go
-import numpy as np
-
 import dash_bootstrap_components as dbc
 
 from settings import config, about
 
+
+import logging
+import sys
+
+
 external_stylesheets = [dbc.themes.CERULEAN]
 
 app = dash.Dash(external_stylesheets=[dbc.themes.CERULEAN])
-server=app.server
+#server=app.server
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 problemas = {
-    'Personal': [u'Una relación amorosa', 'Depresión', 'Desempleo'],
-    'Familiar': [u'Los hijos', 'Los suegros', 'Otros miembros de la familia'],
-    'Laboral': [u'Desmotivación', 'Demasiada presión', 'Falta de oportunidades de crecimiento']
+    'Personal': [u'Con la pareja', 'Con su proyecto de vida', 'Con la percecpión de sí mismo'],
+    'Familiar': [u'Con su posición en la familia', 'En su núcleo familiar : padres e hijos', 'Con la Familia extensa: otros familiares'],
+    'Laboral': [u'Con su jefe(a)', 'Con compañeros de trabajo', 'Con sus Condiciones laborales'],
+    'Social': [u'Amigos', 'Vecinos', 'Relaciones interpersonales en general']
 }
 
 # App Instance
@@ -34,32 +38,23 @@ app.title = config.name
 # Navbar
 navbar = dbc.Nav(className="nav nav-pills", children=[
     ## logo/home
-    dbc.NavItem(html.Img(src=app.get_asset_url("logo.PNG"), height="40px")),
+    #dbc.NavItem(html.Img(src=app.get_asset_url("logo.PNG"), height="40px")),
     ## about
-    dbc.NavItem(html.Div([
-        dbc.NavLink("About", href="/", id="about-popover", active=False),
-        dbc.Popover(id="about", is_open=False, target="about-popover", children=[
-            dbc.PopoverHeader("How it works"), dbc.PopoverBody(about.txt)
-        ])
-    ])),
+            dbc.NavItem(html.Div([
+                #dbc.NavLink("About", href="/", id="about-popover", active=False),
+                dbc.Popover(id="about", is_open=False, target="about-popover", children=[
+                    dbc.PopoverHeader("How it works"), dbc.PopoverBody(about.txt)
+                ])
+            ]))
     ## links
-    dbc.DropdownMenu(label="Links", nav=True, children=[
-        dbc.DropdownMenuItem([html.I(className="fa fa-linkedin"), "  Contacts"], href=config.contacts, target="_blank"),
-        dbc.DropdownMenuItem([html.I(className="fa fa-github"), "  Code"], href=config.code, target="_blank")
-    ])
+    #dbc.DropdownMenu(label="Links", nav=True, children=[
+    #    dbc.DropdownMenuItem([html.I(className="fa fa-linkedin"), "  Contacts"], href=config.contacts, target="_blank"),
+    #    dbc.DropdownMenuItem([html.I(className="fa fa-github"), "  Code"], href=config.code, target="_blank")
+    #])
 ])
 
 # Inputs
 inputs = dbc.FormGroup([
-
-    html.H5(u"Mostrar todas las preguntas?", id= 'titulo_mostrar_preguntas', style={'display':'none'}),
-    dcc.RadioItems(id='mostrar_preguntas', value="",
-                    options=[
-                                {'label': u'Sí', 'value': 'si'},
-                                {'label': 'No', 'value': 'no'}
-                            ] ,
-                    labelStyle={'display': 'none'}
-                   ),
 
     html.Br(),
     html.H5("1. Seleccione tipo de situación", id= 'titulo_tipo_problema', style={'display':'block'}),
@@ -71,8 +66,21 @@ inputs = dbc.FormGroup([
     dcc.RadioItems(id='problema_especifico', value="",
                    labelStyle={'display': 'none'}
                    ),
+
     html.Br(),
-    html.H5(u"3. Está usted en una encrucijada donde tiene que tomar decisiones excluyentes?", id= 'titulo_decision_excluyente', style={'display':'none'}),
+    html.H5(u"3. Qué soluciones ha intentado?", id= 'titulo_soluciones_intentadas', style={'display':'none'}),
+    dcc.RadioItems(id='soluciones_intentadas', value="",
+                    options=[
+                                {'label': 'Dialogar con los involucrados', 'value': 'dialogar'},
+                                {'label': 'Tratar de construir acuerdos', 'value': 'acordar'},
+                                {'label': 'Confrontar a las personas involucradas', 'value': 'confrontar'}
+                            ] ,
+                   labelStyle={'display': 'block'}
+
+                   ),
+
+    html.Br(),
+    html.H5(u"4. Se encuentra usted en la difícil situación en la que debe escoger una sola opción?", id= 'titulo_decision_excluyente', style={'display':'none'}),
     dcc.RadioItems(id='decision_excluyente', value="",
                     options=[
                                 {'label': u'Sí', 'value': 'si'},
@@ -83,7 +91,7 @@ inputs = dbc.FormGroup([
                    ),
 
     html.Br(),
-    html.H5(u"4a. Es esta una decisión que afecta sus principios y valores?", id= 'titulo_decision_afecta_valores', style={'display':'none'}),
+    html.H5(u"5. Es esta una decisión que afecta sus principios y valores?", id= 'titulo_decision_afecta_valores', style={'display':'none'}),
     dcc.RadioItems(id='decision_afecta_valores', value="",
                     options=[
                                 {'label': u'Sí', 'value': 'si'},
@@ -92,9 +100,20 @@ inputs = dbc.FormGroup([
                     labelStyle={'display': 'block'}
                    ),
 
+
     html.Br(),
-    html.H5(u"4b. Es difícil identificar sus objetivos y cómo alcanzarlos?", id= 'titulo_objetivo_dificil', style={'display':'none'}),
-    dcc.RadioItems(id='objetivo_dificil', value="",
+    html.H5(u"6. Es difícil controlar las emociones en esta situación?", id= 'titulo_emociones_desbordadas', style={'display':'none'}),
+    dcc.RadioItems(id='emociones_desbordadas', value="",
+                    options=[
+                                {'label': u'Sí', 'value': 'si'},
+                                {'label': 'No', 'value': 'no'}
+                            ] ,
+                    labelStyle={'display': 'none'}
+                   ),
+
+    html.Br(),
+    html.H5(u"7a. Diría usted que está confundido(a) respecto a cuáles son los objetivos por seguir?", id= 'titulo_identificar_objetivo', style={'display':'none'}),
+    dcc.RadioItems(id='identificar_objetivo', value="",
                     options=[
                                 {'label': u'Sí', 'value': 'si'},
                                 {'label': 'No', 'value': 'no'}
@@ -103,14 +122,34 @@ inputs = dbc.FormGroup([
                    ),
 
     html.Br(),
-    html.H5(u"5. Se encuentra usted en una siuación con emociones desbordadas por usted o alguien involucrado?", id= 'titulo_emociones_desbordadas', style={'display':'none'}),
-    dcc.RadioItems(id='emociones_desbordadas', value="",
+    html.H5(u"7b. Diría usted que no es fácil identificar la forma de alcanzar sus objetivos?", id= 'titulo_alcanzar_objetivo', style={'display':'none'}),
+    dcc.RadioItems(id='alcanzar_objetivo', value="",
                     options=[
                                 {'label': u'Sí', 'value': 'si'},
                                 {'label': 'No', 'value': 'no'}
                             ] ,
-                    labelStyle={'display': 'none'}
+                    labelStyle={'display': 'block'}
                    ),
+
+    html.Br(),
+    html.H5(u"7c. Alguno de los involucrados ha decidido cerrar la comunicación?", id= 'titulo_cerrar_comunicacion', style={'display':'none'}),
+    dcc.RadioItems(id='cerrar_comunicacion', value="",
+                    options=[
+                                {'label': u'Sí', 'value': 'si'},
+                                {'label': 'No', 'value': 'no'}
+                            ] ,
+                    labelStyle={'display': 'block'}
+                   ),
+    html.Br(),
+    html.H5(u"7d. Considera usted que es muy difícil construir acuerdos con los involucrados?", id= 'titulo_construir_acuerdos', style={'display':'none'}),
+    dcc.RadioItems(id='construir_acuerdos', value="",
+                    options=[
+                                {'label': u'Sí', 'value': 'si'},
+                                {'label': 'No', 'value': 'no'}
+                            ] ,
+                    labelStyle={'display': 'block'}
+                   ),
+
 
 
 
@@ -161,6 +200,7 @@ app.layout = dbc.Container(fluid=True, children=[
     ])
 ])
 
+server=app.server
 #####CALLBACKS
 
 
@@ -184,11 +224,11 @@ def def_opciones_prob_especifico(tp):
         return [{'display': 'none'}, {'display': 'none'}]
 
 
-### OCULTAR/MOSTRAR PREGUNTA SOBRE DECISION EXCLUYENTE
+### OCULTAR/MOSTRAR PREGUNTA SOBRE SOLUCIONES INTENTADAS
 @app.callback(
-    Output('decision_excluyente', 'value'),
-    Output('decision_excluyente', 'labelStyle'),
-    Output('titulo_decision_excluyente', 'style'),
+    Output('soluciones_intentadas', 'value'),
+    Output('soluciones_intentadas', 'labelStyle'),
+    Output('titulo_soluciones_intentadas', 'style'),
     Input('tipo_problema', 'value'),
     Input('problema_especifico', 'value')
     )
@@ -199,7 +239,106 @@ def def_mostrar_decision_excluyente(tp, pe): #tp: tipo de problema; de : decisio
         return ['',{'display': 'none'}, {'display': 'none'}]
 
 
+
+### OCULTAR/MOSTRAR PREGUNTA SOBRE DECISION EXCLUYENTE
+@app.callback(
+    Output('decision_excluyente', 'value'),
+    Output('decision_excluyente', 'labelStyle'),
+    Output('titulo_decision_excluyente', 'style'),
+    Input('tipo_problema', 'value'),
+    Input('problema_especifico', 'value'),
+    Input('soluciones_intentadas', 'value')
+    )
+def def_mostrar_decision_excluyente(tp, pe, si): #tp: tipo de problema; de : decision excluyente
+    if tp != '' and pe != '' and si != '':
+        return ['',{'display': 'block'}, {'display': 'block'}]
+    else:
+        return ['',{'display': 'none'}, {'display': 'none'}]
+
+### OCULTAR/MOSTRAR PREGUNTA SOBRE DECISION AFECTA VALORES
+@app.callback(
+    Output('decision_afecta_valores', 'value'),
+    Output('decision_afecta_valores', 'labelStyle'),
+    Output('titulo_decision_afecta_valores', 'style'),
+    Input('tipo_problema', 'value'),
+    Input('problema_especifico', 'value'),
+    Input('soluciones_intentadas', 'value'),
+    Input('decision_excluyente', 'value')
+    )
+def def_mostrar_decision_excluyente(tp, pe, si, de): #tp: tipo de problema; de : decision excluyente
+    if tp != '' and pe != '' and si != '' and de != '':
+        return ['',{'display': 'block'}, {'display': 'block'}]
+    else:
+        return ['',{'display': 'none'}, {'display': 'none'}]
+
+### OCULTAR/MOSTRAR PREGUNTA SOBRE EMOCIONES DESBORDADAS
+@app.callback(
+    Output('emociones_desbordadas', 'labelStyle'),
+    Output('titulo_emociones_desbordadas', 'style'),
+    Input('decision_excluyente', 'value'),
+    Input('decision_afecta_valores', 'value')
+    )
+def def_emociones_desbordadas(de, av):
+    if de == 'no' and av!='' :
+        return [{'display': 'block'}, {'display': 'block'}]
+    else :
+        return [{'display': 'none'}, {'display': 'none'}]
+
+
+### OCULTAR/MOSTRAR PREGUNTA SOBRE IDENTIFICAR OBJETIVO
+@app.callback(
+    Output('identificar_objetivo', 'labelStyle'),
+    Output('titulo_identificar_objetivo', 'style'),
+    Input('emociones_desbordadas', 'value')
+    )
+def def_identificar_objetivo(ed):
+    if ed !='' :
+        return [{'display': 'block'}, {'display': 'block'}]
+    else :
+        return [{'display': 'none'}, {'display': 'none'}]
+
+
+### OCULTAR/MOSTRAR PREGUNTA SOBRE ALCANZAR OBJETIVO
+@app.callback(
+    Output('alcanzar_objetivo', 'labelStyle'),
+    Output('titulo_alcanzar_objetivo', 'style'),
+    Input('identificar_objetivo', 'value')
+    )
+def def_alcanzar_objetivo(io):
+    if io =='no' :
+        return [{'display': 'block'}, {'display': 'block'}]
+    else :
+        return [{'display': 'none'}, {'display': 'none'}]
+
+
+### OCULTAR/MOSTRAR PREGUNTA SOBRE CERRAR COMUNICACION (SOLO PARA CONFLICTO = EMOCION DESBORDADA : SI)
+@app.callback(
+    Output('cerrar_comunicacion', 'labelStyle'),
+    Output('titulo_cerrar_comunicacion', 'style'),
+    Input('emociones_desbordadas', 'value'),
+    Input('alcanzar_objetivo', 'value')
+    )
+def def_cerrar_comunicaciono(ed, ao):
+    if ed=='si' and ao !='' :
+        return [{'display': 'block'}, {'display': 'block'}]
+    else :
+        return [{'display': 'none'}, {'display': 'none'}]
+
+### OCULTAR/MOSTRAR PREGUNTA SOBRE CONSTRUIR ACUERDOS (SOLO PARA PROBLEMA = EMOCION DESBORDADA : NO)
+@app.callback(
+    Output('construir_acuerdos', 'labelStyle'),
+    Output('titulo_construir_acuerdos', 'style'),
+    Input('emociones_desbordadas', 'value'),
+    Input('alcanzar_objetivo', 'value')
+    )
+def def_construir_acuerdos(ed, ao):
+    if ed=='no' and ao !='' :
+        return [{'display': 'block'}, {'display': 'block'}]
+    else :
+        return [{'display': 'none'}, {'display': 'none'}]
+
 ### DECIDIR PREGUNTA: VALORES U OBJETIVO DEPENDIENDO DE DECISION EXCLUYENTE
+'''
 @app.callback(
     Output('titulo_decision_afecta_valores', 'style'),
     Output('decision_afecta_valores', 'labelStyle'),
@@ -216,23 +355,8 @@ def def_mostrar_valores_objetivo(tp, pe, de):
         return {'display': 'block'}, {'display': 'block'}, {'display': 'none'}, {'display': 'none'}
     if de == 'no':
         return {'display': 'none'}, {'display': 'none'}, {'display': 'block'}, {'display': 'block'}
+'''
 
-
-### 5. OCULTAR/MOSTRAR PREGUNTA SOBRE EMOCIONES DESBORDADAS
-@app.callback(
-    Output('emociones_desbordadas', 'labelStyle'),
-    Output('titulo_emociones_desbordadas', 'style'),
-    Input('decision_excluyente', 'value'),
-    Input('decision_afecta_valores', 'value'),
-    Input('objetivo_dificil', 'value')
-    )
-def def_emociones_desbordadas(de, av, od):
-    if od != '' :
-        return [{'display': 'block'}, {'display': 'block'}]
-    elif de == 'si' and av =='no':
-        return [{'display': 'block'}, {'display': 'block'}]
-    elif od == '' or av =='':
-        return [{'display': 'none'}, {'display': 'none'}]
 
 
 ### DIAGNOSTICO
@@ -243,17 +367,19 @@ def def_emociones_desbordadas(de, av, od):
     Input('problema_especifico', 'value'),
     Input('decision_excluyente', 'value'),
     Input('decision_afecta_valores', 'value'),
-    Input('emociones_desbordadas', 'value')
+    Input('emociones_desbordadas', 'value'),
+    Input('identificar_objetivo', 'value'),
+    Input('alcanzar_objetivo', 'value')
     )
-def def_diagnostico(tp, pe, de, av, ed):
-    print (tp, pe, de, av)
-    if tp != '' and pe != ''and de == 'si' and av== 'si':
+def def_diagnostico(tp, pe, de, av, ed,io, ao):
+    #print ( tp, pe, de, av,ed,io, ao)
+    if tp != '' and pe != ''and de == 'si' and av != '':
         return {'display': 'block','width': '100%', 'height': 100, 'font-size': '2rem', 'margin-top': '40px' }, u'Usted está enfrentando un Dilema'
-    elif tp != '' and pe != '' and ed== 'si':
+    elif tp != '' and pe != '' and de == 'no' and ed== 'si' and io != '':
         return {'display': 'block','width': '100%', 'height': 100, 'font-size': '2rem' , 'margin-top': '40px'}, u'Usted está enfrentando un Conflicto'
-    elif tp != '' and pe != '' and ed== 'no':
+    elif tp != '' and pe != '' and de == 'no' and ed== 'no' and io != '':
         return {'display': 'block','width': '100%', 'height': 100, 'font-size': '2rem' , 'margin-top': '40px'}, u'Usted está enfrentando un Problema'
-    elif tp != '' and pe != ''or de == 'no' or av== 'no':
+    elif tp != '' and pe != ''or de != '' :
         return {'display': 'none','width': '100%', 'height': 100, 'font-size': '2rem' , 'margin-top': '40px'}, u''
 
 ### DESCRIPCION DEL PROBLEMA
@@ -263,16 +389,34 @@ def def_diagnostico(tp, pe, de, av, ed):
     Input('problema_especifico', 'value'),
     Input('decision_excluyente', 'value'),
     Input('decision_afecta_valores', 'value'),
-    Input('emociones_desbordadas', 'value')
+    Input('emociones_desbordadas', 'value'),
+    Input('identificar_objetivo', 'value'),
+    Input('alcanzar_objetivo', 'value')
     )
-def def_descripcion_problema(tp, pe, de, av, ed):
-    if tp != '' and pe != '' and de == 'si' and av == 'si':
-        return u'Usted ha señalado que tiene una situación {} relacionada con  {}.\
-             Esta situación  {} lo(a) obliga a tomar decisiones excluyentes y {} afecta sus valores.'.format(tp.lower(), pe.lower(), de.lower(), av.lower())
-    elif tp != '' and pe != ''and de == 'no' and av!='' and ed!= '':
-        return u'Usted ha señalado que tiene una situación {} relacionada con  {}.\
-                Esta situación  {} lo(a) obliga a tomar decisiones excluyentes y {} afecta sus valores.\
-                La situación {} ha implicado desbordamiento de emociones'.format(tp.lower(), pe.lower(), de.lower(),ed.lower())
+def def_descripcion_problema(tp, pe, de, av, ed ,io, ao):
+    if tp != '' and pe != ''and de == 'si' and av != '':
+            return u'Usted ha señalado que tiene una situación {}\
+                relacionada   {}.\
+                Esta situación  {} lo(a) obliga a tomar decisiones excluyentes y \
+                {} afecta sus valores.'.format(tp.lower(), pe.lower(), de.lower(), av.lower())
+
+    elif tp != '' and pe != '' and de == 'no' and av != '' and ed== 'si'  and io != '':
+            return  u'Usted ha señalado que tiene una situación {}\
+                    relacionada   {}.\
+                    Esta situación  {} lo(a) obliga a tomar decisiones excluyentes y \
+                    {} afecta sus valores.\
+                    La situación {} ha implicado desbordamiento de emociones y {} hace que sea difícil identificar sus objetivos.'\
+                    .format(tp.lower(), pe.lower(), de.lower(), av.lower(), ed.lower(),io.lower())
+
+    elif tp != '' and pe != '' and de == 'no' and av != '' and ed== 'no'  and io != '':
+            return u'Usted ha señalado que tiene una situación {}\
+                    relacionada   {}.\
+                    Esta situación  {} lo(a) obliga a tomar decisiones excluyentes y {} ha hecho que sea difícil identificar sus objetivos.\
+                    La situación {} ha implicado desbordamiento de emociones'\
+                    .format(tp.lower(), pe.lower(), de.lower(), av.lower(), ed.lower(),io.lower())
+
+    elif tp != '' and pe != ''or de == 'no' or av== 'no':
+        return ''
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, dev_tools_ui=False)
